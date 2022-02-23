e5b08b0a98121e6ffbf6dbc3333da45a9b175fc2
zhangheng <1049725878@qq.com>
2022-02-23 21:27:30 +0800

    a

--- src/main/java/com/zh/Main.java
+++ src/main/java/com/zh/Main.java
@@ -1,9 +1,27 @@
 package com.zh;
 
+
+import com.zh.commit.PostCommit;
+
+import java.util.Arrays;
+
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        if (args.length < 1)
+            return;
+
+        if ("--PC".equalsIgnoreCase(args[0])) {
+            String[] strs = new String[args.length - 1];
+            System.arraycopy(args, 1, strs, 0,
+                    args.length - 1);
+            PostCommit pc = new PostCommit(strs);
+        }
+
+
     }
+
+
 
 }
---
+++ src/main/java/com/zh/commit/PostCommit.java
@@ -0,0 +1,11 @@
+package com.zh.commit;
+
+/**
+ * commit 后执行
+ */
+public class PostCommit {
+
+    public PostCommit(String[] args) {
+
+    }
+}
